bd9f3cad2628eb8451526d478409190f9d75fa8e
lei dong <lei.dong@infosecglobal.com>
2022-08-10 11:44:40 -0400

    apply tcf to ccac compiling

--- cmake-arc-baremetal.sh
+++ cmake-arc-baremetal.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ -z $1 ]; then echo "$0 <path to root folder of CMakeList.txt>"; exit 1; fi
-SRC_DIR=$(realpath $1)
+SRC_DIR=$(dirname $(realpath $0))
+if [ $SRC_DIR = $(pwd) ]; then echo "this should be run in build dir"; exit 1; fi
 
+#ARC_ROOT=/cyg/opt/ARC/MetaWare/arc
+#ARC_ROOT=/cygdriver/ARC/MetaWare/arc
 ARC_ROOT=/opt/ARC/MetaWare/arc
 ARC_TOOLCHAIN_CMAKE=${ARC_ROOT}/cmake/arc-mwdt.toolchain.cmake
 ARC_TCF=${ARC_ROOT}/tcf/em4_ecc.tcf
@@ -12,11 +14,23 @@ CMAKE_DEFS="${CMAKE_DEFS} -DARC_CFG_TCF_PATH=${ARC_TCF}"
 CMAKE_DEFS="${CMAKE_DEFS} -DOQS_ENABLE_KEM_SIKE=OFF -DOQS_ENABLE_KEM_SIDH=OFF"
 CMAKE_DEFS="${CMAKE_DEFS} -DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON"
 CMAKE_DEFS="${CMAKE_DEFS} -DOQS_USE_OPENSSL=OFF"
-CMAKE_DEFS="${CMAKE_DEFS} -DOQS_BUILD_ONLY_LIB=ON"
+CMAKE_DEFS="${CMAKE_DEFS} -DCMAKE_BUILD_TYPE=Debug"
+CMAKE_DEFS="${CMAKE_DEFS} -DOQS_OPT_TARGET=generic"
+#CMAKE_DEFS="${CMAKE_DEFS} -DOQS_BUILD_ONLY_LIB=ON"
 
-LOG_DIR="../log/"
+LOG_DIR=$(realpath "../log")
+[ -d ]
 LOG_FILE="${LOG_DIR}/cmake-arc-oqs-ninja-noopenssl.log"
+CMAKE_GEN=Ninja
+BUILD_DIR=
 
 echo "cmake parms = ${CMAKE_DEFS}"
-cmake --debug --trace-expand -GNinja ${CMAKE_DEFS} ${SRC_DIR} 2>&1 | tee ${LOG_FILE}
+echo "ARC_TCF = ${ARC_TCF}"
+echo "ARC_TOOLCHAIN = ${ARC_TOOLCHAIN_CMAKE}"
+echo "LOG_FILE = ${LOG_FILE}"
+echo "Generate = ${CMAKE_GEN}"
+read -p "Continue cmake build generate ? (y/n) " rc
+[ $rc = y ] || exit 1
+cmake --debug-output --trace-expand -G${CMAKE_GEN} ${CMAKE_DEFS} ${SRC_DIR} 2>&1 | tee ${LOG_FILE}
+exit 0
 
